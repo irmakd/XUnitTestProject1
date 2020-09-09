b1503e99677e5921eb6884a9361363f41eb17743
irmakd <irmak.degerlier@gmail.com>
2020-09-09 19:25:48 +0300

    Add project files.

---
+++ Calculator/SalaryCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class SalaryCalculator
+    {
+ 
+        private const int HoursInYear = 2340;
+        //public decimal GetAnnualSalary(decimal hourlyWage)
+        //{
+        //    return hourlyWage * HoursInYear;
+        //}
+        public decimal GetAnnualSalary(decimal hourlyWage) =>  hourlyWage * HoursInYear;
+
+        //public decimal GetHourlyWage(decimal annualSalary)
+        //{
+        //    decimal hourlyWage = annualSalary / HoursInYear;
+        //    return hourlyWage;
+
+        //}
+
+        public decimal GetHourlyWage(decimal annualSalary) =>  annualSalary / HoursInYear;
+     
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Calculator;
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Hazırlama - Arrange
+            SalaryCalculator salaryCalculator = new SalaryCalculator();
+
+            //Hareket - Act
+            decimal annualSalary = salaryCalculator.GetAnnualSalary(40);
+            //İddia etme - Assert
+            Assert.Equal(93600, annualSalary);
+
+        }
+        [Fact]
+        public void Test2()
+        {
+            //Hazırlama - Arrange
+            SalaryCalculator salaryCalculator = new SalaryCalculator();
+
+            //Hareket - Act
+            decimal hourlyWage = salaryCalculator.GetHourlyWage(93600);
+            //İddia etme - Assert
+            Assert.Equal(40, hourlyWage);
+
+        }
+    }
+}
